2a34035d529aeaee675c7b8149e0f7d383fce6c4
Gabsousa2203 <gadeso2003@gmail.com>
2025-02-06 17:22:41 -0400

    feat: :sparkles: Implement file upload and retrieval functionality

---
+++ src/files/files.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Post, Param, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilterHelper,fileNamer} from './helpers';
+import { diskStorage } from 'multer';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('files')
+export class FilesController {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly filesService: FilesService
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response, //*Aqui se le dice a nest que Yo voy a manejar la respuesta
+    @Param('imageName') imageName: string
+  ){
+    
+    let path = this.filesService.getStaticProductImage(imageName);
+
+    res.sendFile( path );
+  }
+
+  @Post("product")
+  @UseInterceptors( FileInterceptor('file',
+    { fileFilter: fileFilterHelper,
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer
+      })
+     }
+  ) )
+  uploadProductImage( 
+    @UploadedFile() file: Express.Multer.File ) {
+    
+      if ( !file ) 
+        throw new BadRequestException('Make sure that file is an image');
+    
+      const secureUrl = `${ this.configService.get('HOST_API') }/files/product/${file.filename}`;
+
+      return {secureUrl};
+  }
+
+}
---
+++ src/files/helpers/index.ts
@@ -0,0 +1,4 @@
+export { fileFilterHelper } from "./file-filter-helper";
+export { fileNamer } from "./file-namer-helper";
+
+
